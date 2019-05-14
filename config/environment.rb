80138ca1c04ce85d487fc2f40eb3824f2038b358
Garth Lilly <garff247@gmail.com>
2019-05-14 04:21:46 +0000

    Automatically backed up by Learn

--- config/environment.rb
+++ config/environment.rb
@@ -5,7 +5,7 @@ module Concerns
   module Findable
     
     def find_by_name(name)
-      @@all.detect(name) {|song| song.name == name}
+      Song.all.detect(name) {|song| song.name == name}
     end
   end
 end
