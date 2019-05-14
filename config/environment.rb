6e564bab5fbd6b8e34ed2ae31169a7563f701cac
Garth Lilly <garff247@gmail.com>
2019-05-14 06:30:45 +0000

    Automatically backed up by Learn

--- config/environment.rb
+++ config/environment.rb
@@ -12,7 +12,7 @@ module Concerns
     def find_or_create_by_name(name)
 
       x = self.find_by_name(name)
-      if  self.find_by_name(name) == nil
+      if  find_by_name(name) == nil
         puts name
         self.create(name)
 
