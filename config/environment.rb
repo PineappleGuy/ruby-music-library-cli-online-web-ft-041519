d4e76100238a7d8509da0e6bbbb127e918f2e914
Garth Lilly <garff247@gmail.com>
2019-05-14 04:49:10 +0000

    Automatically backed up by Learn

--- config/environment.rb
+++ config/environment.rb
@@ -6,6 +6,15 @@ module Concerns
     def find_by_name(name)
       self.all.detect(name) {|x| x.name == name}
     end
+
+    def find_or_create_by_name(name)
+      if self.all.include?(name) == false
+        self.all << name
+      else
+        self.all.detect(name) {|x| x.name == name}
+      end
+    end
+
   end
 end
 
