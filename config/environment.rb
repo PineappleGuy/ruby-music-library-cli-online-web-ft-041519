c58f79c587c82c18a596f644a941d813d713bc8f
Garth Lilly <garff247@gmail.com>
2019-05-14 06:43:20 +0000

    Automatically backed up by Learn

--- config/environment.rb
+++ config/environment.rb
@@ -6,16 +6,13 @@ module Concerns
   module Findable
 
     def find_by_name(name)
-      self.all.detect(name) {|x| x.name == name}
+      all.find do |song|
+        song.name == name
+      end
     end
 
     def find_or_create_by_name(name)
-      Song.all.each do |song|
-        if song.name == name
-          return song
-        end
-      end
-        Song.create(name)
+      find_by_name || self.create(name)
     end
 
   end
