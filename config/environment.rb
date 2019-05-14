e64dee28050e31667a8938f32248759f975dfd08
Garth Lilly <garff247@gmail.com>
2019-05-14 06:56:04 +0000

    Automatically backed up by Learn

--- config/environment.rb
+++ config/environment.rb
@@ -6,7 +6,7 @@ module Concerns
   module Findable
 
     def find_by_name(name)
-      self.all.detect(name) {|x| x.name == name}
+      self.all.detect {|x| x.name == name}
     end
 
     def find_or_create_by_name(name)
